d0d604e6d9d00796307f5750b400ffd5e805d75a
rydnr <github@acm-sl.org>
2023-08-24 18:03:44 +0200

    Updated to pythoneda-realm-rydnr/events 0.0.1a7

--- events/flake.nix
+++ events/flake.nix
@@ -22,13 +22,13 @@
     nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
     pythoneda-shared-pythoneda-banner = {
-      url = "github:pythoneda-shared-pythoneda/banner/0.0.1a13";
+      url = "github:pythoneda-shared-pythoneda/banner/0.0.1a16";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
     };
     pythoneda-shared-pythoneda-domain = {
       url =
-        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a33?dir=domain";
+        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a37?dir=domain";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
@@ -41,8 +41,8 @@
       let
         org = "pythoneda-realm-rydnr";
         repo = "events";
-        version = "0.0.1a6";
-        sha256 = "sha256-/M3djTjEneiARQvIiLui94pI/q+4VBh6xlSJOrQs0zU=";
+        version = "0.0.1a7";
+        sha256 = "sha256-nk67pq3Eg9YEI9Sm9ytWUlWqnmfRIJXBu5cQVMfFJ74=";
         pname = "${org}-${repo}";
         pkgs = import nixos { inherit system; };
         description = "Events for pythoneda-realm-rydnr/realm";
@@ -95,7 +95,7 @@
             propagatedBuildInputs = with python.pkgs;
               [ pythoneda-shared-pythoneda-domain ];
 
-            pythonImportsCheck = [ pythonpackage ];
+            # pythonImportsCheck = [ pythonpackage ];
 
             unpackPhase = ''
               cp -r ${src} .
