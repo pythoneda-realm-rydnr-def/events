9963a250ec7eb0cd445af975dfa0971665c09a32
rydnr <github@acm-sl.org>
2023-09-16 17:44:39 +0200

    Updated pythoneda-shared-pythoneda/domain to 0.0.1a41

--- events/flake.nix
+++ events/flake.nix
@@ -28,7 +28,7 @@
     };
     pythoneda-shared-pythoneda-domain = {
       url =
-        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a40?dir=domain";
+        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a41?dir=domain";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
